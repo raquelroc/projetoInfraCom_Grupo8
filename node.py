320b046479a320207d758a2a4bdf4355dd4cc529
j0rgejuni0r <juniorjorge0375@gmail.com>
2024-07-17 15:52:07 -0300

    feat: hash table e GET
    - Função PUT consertada
    - criação da hash function
    - criação da função GET, porém sem o retorno da busca para o nó que solicitou originalmente

--- node.py
+++ node.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import hashlib
+import time
 
 tabelaRot = {
     1: {"vizinhos": [5, 2], "host": "127.0.0.1", "port": 1231},
@@ -9,6 +11,21 @@ tabelaRot = {
     5: {"vizinhos": [4, 1], "host": "127.0.0.5", "port": 1235}
 }
 
+def hash_key(key, lower_bound=0, upper_bound=5, decimals=2):
+
+    # Gera um hash em int
+    hashInt = int(hashlib.sha1(key.encode()).hexdigest(), 16) 
+
+    # normaliza o valor para o intervalo [0, 1)
+    normalized_value = hashInt / float(2**160) 
+
+    # escala o valor para o intervalor [0, 5]
+    scaled_value = lower_bound + (upper_bound - lower_bound) * normalized_value
+
+    # arredonda o valor para duas casas decimais
+    rounded_value = round(scaled_value, decimals) 
+
+    return rounded_value
 
 threads = [] # usado para finalizar as threads
 
@@ -23,7 +40,7 @@ class node:
         # criação dos atributos do nó
         self.identifier = nodeID
         self.nodeInfo = tabelaRot[nodeID]
-        self.hashTable = self.create_hash_table()
+        self.hashTable = {} # a hashTable do nó possui as chaves do intervalo n-1 até n menos o n 
 
         # Iniciando o server do nó
         self.serverThread = threading.Thread(target=self.run_server)
@@ -32,16 +49,6 @@ class node:
         threads.append(self.serverThread) # para o problema com as threads
 
         self.serverThread.start()
-
-    # Função para criaçao da hashtable
-    # - a hash table armazena apenas os dados "host" e "port" dos vizinhos desse nó
-    def create_hash_table(self):
-        hash_table = {}
-
-        for vizinho in self.nodeInfo["vizinhos"]:
-            hash_table[vizinho] = (tabelaRot[vizinho]["host"], tabelaRot[vizinho]["port"])
-        
-        return hash_table
 
     # Funçao para criação do servidor desse nó
     def run_server(self):
@@ -58,9 +65,6 @@ class node:
             nodeSock, nodeAddr = sock.accept()
 
             # dentro de nodeSock é possivel ver as informações de host e port que nós escolhemos
-            #print(nodeAddr, nodeSock)
-            #print(f"aqui {nodeSock.getpeername()}")
-
             clientThread = threading.Thread(target=self.handle_client, args=(nodeSock, nodeAddr))
             clientThread.daemon = True
             
@@ -76,54 +80,107 @@ class node:
         # exemplo de comando enviado por pacote "PUT3" -> comando + key
         # caso um pacote chegue com o comando put a função put é chamada
         if arquivo[0:2] in "PUT":
-            self.put(int(arquivo[3]))
+            self.put(int(arquivo[3]) + int(arquivo[5]) * 0.1 + int(arquivo[6]) * 0.01)
+        
+        if arquivo[0:2] in "GET":
+            self.get(int(arquivo[3]) + int(arquivo[5]) * 0.1 + int(arquivo[6]) * 0.01)
+            
 
         # pacotes normais
-        print(f"o no {self.identifier} recebeu um arquivo do endereco {nodeAddr}: {arquivo}")
+        print(f"o no {self.identifier} recebeu um arquivo do endereco {nodeSock.getsockname()}: {arquivo}")
         nodeSock.close()
 
-    # Função para a função PUT *não funcionando*
-    # - caso o nó atual não seja o desejado ele irá identificar qual vizinho
-    # - é o melhor para enviar o pacote e mandara um pacote com o comando para o vizinho
-    # PROBLEMA : o programa fica em um loop nos nós 5 e 1 devido a decisão baseada no maior/menor
+    # Função para a função PUT
+    # - caso o nó atual não seja o desejado ele irá identificar 
+    # - qual vizinho está mais perto do intervalo desejado
     def put(self, key):
         vizinhoId = 0
 
-        # verificando se o nó atual é o certo
-        if self.identifier == key:
-            print("a")
+        # caso onde a key esteja no intervalo
+        if key < self.identifier and key >= self.nodeInfo["vizinhos"][0]:
+            self.hashTable[key] = 'teste'
+            print(f"o nó {self.identifier} armazenou o arquivo 'teste' com a chave {key}")
             return
-        
-        # decidindo para qual vizinho enviar o pacote
-        elif key < self.identifier:
-            vizinhoId = self.hashTable[min(self.hashTable.keys())]
-        elif key > self.identifier:
-            vizinhoId = self.hashTable[max(self.hashTable.keys())]
+
+        # caso tratando a conexão 5 - 1
+        if self.identifier == 1:
+            if key < self.identifier or key > self.nodeInfo["vizinhos"][0]:
+                self.hashTable[key] = 'teste'
+                print(f"o nó {self.identifier} armazenou o arquivo 'teste' com a chave {key}")
+                return
+
+        if key >= self.identifier:
+            vizinhoId = self.nodeInfo["vizinhos"][1]
+
+        if key < self.nodeInfo["vizinhos"][0]:
+            vizinhoId = self.nodeInfo["vizinhos"][0]
 
         # enviando o pacote
         try:
             maiorSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            maiorSock.connect((vizinhoId[0], vizinhoId[1]))
+            maiorSock.connect((tabelaRot[vizinhoId]["host"], tabelaRot[vizinhoId]["port"]))
 
             arquivo = f"PUT{key}"
 
             maiorSock.send(arquivo.encode())
             maiorSock.close()
         except ConnectionRefusedError:
-                print(f"o no {max(self.hashTable.keys())} nao esta disponivel")
+                print(f"o no {vizinhoId} nao esta disponivel")
         except Exception as e:
-            print(e)
-            print(f"erro ao enviar arquivo do no {nodeID} para o no {max(self.hashTable.keys())}")
+            print(f"aqui {e}")
+            print(f"erro ao enviar arquivo do no {nodeID} para o no {vizinhoId}")
+
+    # Função para a função GET *não esta funcinando*
+    # - caso o nó atual não seja o desejado ele irá identificar 
+    # - qual vizinho está mais perto do intervalo desejado 
+    # PROBLEMA: ainda não foi implementado o retorno da busca 
+    # - para o nó que fez a solicitação inicialmente      
+    def get(self, key):
+        vizinhoId = 0
+
+        # caso onde a key esteja no intervalo
+        if key < self.identifier and key >= self.nodeInfo["vizinhos"][0]:
+            if key in self.hashTable.keys():
+                return self.hashTable[key]
+            else:
+                return "chave não existente"
             
+        # caso tratando a conexão 5 - 1
+        if self.identifier == 1:
+            if key < self.identifier or key > self.nodeInfo["vizinhos"][0]:
+                if key in self.hashTable.keys():
+                    return self.hashTable[key]
+                else:
+                    return "chave não existente"
+        
+        if key >= self.identifier:
+            vizinhoId = self.nodeInfo["vizinhos"][1]
+
+        if key < self.nodeInfo["vizinhos"][0]:  
+            vizinhoId = self.nodeInfo["vizinhos"][0]
+
+        try:
+            menorSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            menorSock.connect((tabelaRot[vizinhoId]["host"], tabelaRot[vizinhoId]["port"]))
+
+            arquivo = f"GET{key}"
+
+            menorSock.send(arquivo.encode())
+            menorSock.close()
+        except ConnectionRefusedError:
+                print(f"o no {vizinhoId} nao esta disponivel")
+        except Exception as e:
+            print("e")
+            print(f"erro ao enviar arquivo do no {nodeID} para o no {vizinhoId}")
 
     # Função para testar a conexão entre os vizinhos
     # - cada nó envia um pacote com uma mensagem para seus vizinhos imediatos
     def send_hi(self):
         for vizinhoID in self.nodeInfo["vizinhos"]:
-            vizinhoInfo = self.hashTable[vizinhoID]
+            vizinhoInfo = self.nodeInfo["vizinhos"]
             try:
                 vizinhoSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                vizinhoSock.connect((vizinhoInfo[0], vizinhoInfo[1]))
+                vizinhoSock.connect((tabelaRot[vizinhoID]["host"], tabelaRot[vizinhoID]["port"]))
 
                 arquivo = f"nome arquivo, enviado por {self.identifier}"
 
@@ -133,6 +190,7 @@ class node:
             except ConnectionRefusedError:
                 print(f"o no {vizinhoID} nao esta disponivel")
             except Exception as e:
+                print(e)
                 print(f"erro ao enviar arquivo do no {nodeID} para o no {vizinhoID}")
     
     
@@ -144,11 +202,19 @@ for nodeID in tabelaRot.keys():
     nodes.append(nodeBase)
 
 # teste para a conexão dos nós
-for realnode in nodes:
+"""for realnode in nodes:
     realnode.send_hi()
+"""
+# teste para o funcionamento da função PUT
+print(hash_key("teste"))
+nodes[1].put(hash_key("teste"))
 
-# teste para o funcionamento da função PUT *não funcionando*
-nodes[0].put(2)
+time.sleep(1)
+
+# teste para o funcionamento da função GET
+print(nodes[0].hashTable)
+print(nodes[1].get(hash_key("teste")))
+
 
 # verificar criação das hash tables
 """for realnode in nodes:
