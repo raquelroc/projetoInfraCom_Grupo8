1dd27d75eb005947230c27074a482704612fdcd5
j0rgejuni0r <juniorjorge0375@gmail.com>
2024-07-19 18:16:47 -0300

    feat: implementação da finger table
    - separação em dois arquivos, um para a busca sequencial e outra para a finger table
    - implementação da finger table
    - comando PUT para ambas as versões do código

---
+++ nodeft.py
@@ -0,0 +1,164 @@
+import socket
+import threading
+import hashlib
+import time
+
+tabelaRot = {
+    1: {"vizinhos": [5, 2], "host": "127.0.0.1", "port": 1231},
+    2: {"vizinhos": [1, 3], "host": "127.0.0.2", "port": 1232},
+    3: {"vizinhos": [2, 4], "host": "127.0.0.3", "port": 1233},
+    4: {"vizinhos": [3, 5], "host": "127.0.0.4", "port": 1234},
+    5: {"vizinhos": [4, 1], "host": "127.0.0.5", "port": 1235}
+}
+
+threads = [] # usado para finalizar as threads
+stop_event = threading.Event()
+
+def hash_key(key, lowerBound=0, upperBound=5, decimals=2):
+
+    # Gera um hash em int
+    hashInt = int(hashlib.sha1(key.encode()).hexdigest(), 16) 
+    # normaliza o valor para o intervalo [0, 1)
+    normalizedValue = hashInt / float(2**160) 
+    # escala o valor para o intervalor [0, 5]
+    scaledValue = 0 + (upperBound - lowerBound) * normalizedValue
+    # arredonda o valor para duas casas decimais
+    roundedValue = round(scaledValue, decimals) 
+
+    return roundedValue
+
+# OBS:
+# - Ao rodar o programa ele fica aberto, para rodar novamente feche o terminal 
+# - fiz isso para evitar um problema onde as threads daemon ainda estavam
+# - em execução enquanto o interpretador Python finalizava 
+class node:
+    def __init__(self, nodeID):
+        # criação dos atributos do nó
+        self.identifier = nodeID
+        self.nodeInfo = tabelaRot[nodeID]
+        self.hashTable = {} # a hashTable do nó possui as chaves do intervalo n-1 até n menos o n 
+        self.fingerTable = self.create_figerTable()
+
+        # Iniciando o server do nó
+        self.serverThread = threading.Thread(target=self.run_server)
+        self.serverThread.daemon = True
+
+        threads.append(self.serverThread) # para o problema com as threads
+
+        self.serverThread.start()
+
+    def create_figerTable(self):
+        tempFingerTable = {}
+        for i in range(3):
+            row = [i+1, self.identifier + 2 ** i]
+            if row[1] > 5:
+                row[1] -= 5
+            tempFingerTable[row[0]] = row[1]
+        return tempFingerTable
+
+    # Funçao para criação do servidor desse nó
+    def run_server(self):
+        # Criação do server do nó
+        host, port = self.nodeInfo["host"], self.nodeInfo["port"]
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((host, port))
+        sock.listen(5)
+
+        print(f"No {self.identifier} escutando em {host}: {port}")
+        
+        # recebimento dos pacotes, são tratados no handle_client
+        while True:
+            nodeSock, nodeAddr = sock.accept()
+
+            # dentro de nodeSock é possivel ver as informações de host e port que nós escolhemos
+            clientThread = threading.Thread(target=self.handle_client, args=(nodeSock,))
+            clientThread.daemon = True
+            
+            threads.append(clientThread) # para o problema com as threads
+
+            clientThread.start()
+    
+    # Função de tratamento dos pacotes recebidos
+    def handle_client(self, nodeSock):
+        try:
+            arquivo = nodeSock.recv(1024).decode()
+            
+            # tratando casos de chaves inteiros
+            requestedKey = 0
+            requestedKey += int(arquivo[3])
+            if len(arquivo) > 4 : requestedKey += int(arquivo[5]) * 0.1
+            if len(arquivo) > 5 : requestedKey += int(arquivo[6]) * 0.01
+
+            # comandos
+            # exemplo de comando enviado por pacote "PUT3" -> comando + key
+            # caso um pacote chegue com o comando put a função put é chamada
+            match arquivo[0:3]:
+                case "PUT":
+                    self.put(requestedKey)
+
+        finally:
+            # pacotes normais
+            print(f"o no {self.identifier} recebeu um arquivo do endereco {nodeSock.getsockname()}: {arquivo}")
+            nodeSock.close()
+
+    #Função que manda pacotes responsaveis por comando para outros nós
+    def send_command(self, id, key, command):
+        try:
+            Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            Sock.connect((tabelaRot[id]["host"], tabelaRot[id]["port"]))
+
+            arquivo = f"{command}{key}"
+
+            Sock.send(arquivo.encode())
+            Sock.close()
+        except ConnectionRefusedError:
+                print(f"o no {id} nao esta disponivel")
+        except Exception as e:
+            print(f"aqui {e}")
+            print(f"erro ao enviar arquivo do no {nodeID} para o no {id}")
+
+    # Função que verifica se a chave está no intervalo desse nó
+    def in_interval(self, key, limiteInferior, limiteSuperior):
+        if self.identifier == 1 and (key < 1 or key > 5):
+            return True
+        return limiteInferior <= key < limiteSuperior
+
+    # Função PUT utilizando a finger table
+    def put(self, key):
+        if self.in_interval(key, self.identifier-1, self.identifier-1 + 0.99):
+            print(f"o nó {self.identifier} armazenou o arquivo 'teste' com a chave {key}")
+            self.hashTable[key] = 'teste'
+            return
+
+        nextNode = 6
+        for node in self.fingerTable.values():
+            if abs(node - key) < nextNode:
+                nextNode = node
+
+        self.send_command(nextNode, key, "PUT")   
+
+# Criação dos 5 nós
+nodes = []
+for nodeID in tabelaRot.keys():
+    nodeBase = node(nodeID)
+    nodes.append(nodeBase)
+
+
+# teste para o funcionamento da função PUT
+nodes[0].put2(1)
+
+time.sleep(1)
+
+try:
+    while True:
+        time.sleep(3)
+except KeyboardInterrupt:
+    stop_event.set()
+    print("Finalizando threads...")
+
+# para o problema com as threads
+for thread in threads:
+    thread.join()
+
+print("aquif")
+
